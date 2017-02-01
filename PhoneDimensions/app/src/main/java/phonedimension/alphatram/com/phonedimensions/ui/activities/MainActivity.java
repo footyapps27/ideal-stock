5d87ffc0dbe1c64cf9efe36a1d4728fa5a8784bf
Samrat <samrat2702@gmail.com>
2017-02-02 01:07:18 +0800

    Tab layout completed.

--- PhoneDimensions/app/src/main/java/phonedimension/alphatram/com/phonedimensions/ui/activities/MainActivity.java
+++ PhoneDimensions/app/src/main/java/phonedimension/alphatram/com/phonedimensions/ui/activities/MainActivity.java
@@ -1,11 +1,14 @@
 package phonedimension.alphatram.com.phonedimensions.ui.activities;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TableLayout;
 
 import butterknife.BindView;
 import phonedimension.alphatram.com.phonedimensions.R;
@@ -19,15 +22,22 @@ public class MainActivity extends BaseActivity {
     /*********************************************/
     @BindView(R.id.main_view_pager)
     ViewPager mViewPager;
-
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.tab_layout)
+    TabLayout mTabLayout;
     /*********************************************/
     // Activity Lifecycle
     /*********************************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setUpViewPager();
-        // Setup the view pager
+        // Add toolbar
+        setSupportActionBar(mToolbar);
+        // Setup the tab layout
+        setupTabLayout();
+        // Setup view pager
+        setupViewPager();
     }
 
     /*********************************************/
@@ -45,7 +55,8 @@ public class MainActivity extends BaseActivity {
     /**
      * Method to setup the view pager.
      */
-    private void setUpViewPager() {
+    private void setupViewPager() {
+        // Setup Pager
         FragmentManager fragmentManager = getSupportFragmentManager();
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
@@ -58,14 +69,49 @@ public class MainActivity extends BaseActivity {
                         break;
                     case 3:
                         break;
+                    default:
+                        break;
                 }
                 return new AboutUsFragment();
             }
 
             @Override
             public int getCount() {
-                return 3;
+                return mTabLayout.getTabCount();
+            }
+        });
+        // Attach the listeners to the tab layout
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+    }
+
+    /**
+     * Method to setup the tab layout
+     */
+    private void setupTabLayout() {
+        // Add the tabs
+        // TODO: - Replace this with icons
+        mTabLayout.addTab(mTabLayout.newTab().setText("About Us"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("All dimensions"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("Add"));
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // Add the listener
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
     }
+
 }
